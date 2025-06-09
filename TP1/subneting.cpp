4402f75728a1b3cfd62241ce1fc2dba66c92eaf8
joseaguerrar <joseaguerrar2017@gmail.com>
2025-06-09 16:16:29 -0600

    Carpeta con c++ inicial de la tarea

---
+++ TP1/subneting.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[] )
+{
+    if (argc == 1){
+        cout << "Por favor ingrese su solicitud de subneteo IPv4 de esta manera:" << endl;
+    }
+    return 0;
+}
